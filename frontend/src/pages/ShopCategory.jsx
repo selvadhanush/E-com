7579d554262b3abda5ad20ec7f89a872cd026251
selvadhanush <selva.dhanush.s81@kalvium.community>
2025-09-24 17:53:33 +0530

    done

--- frontend/src/pages/ShopCategory.jsx
+++ frontend/src/pages/ShopCategory.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { ShopContext } from "../context/ShopContext";  
 import "./CSS/ShopCategory.css";
 import drop_icon from "../Components/Assets/Frontend_Assets/dropdown_icon.png";
-
+import Item from "../Components/Item/Item";
 function ShopCategory(props) {
   const { all_products } = useContext(ShopContext);
 
@@ -18,7 +18,14 @@ function ShopCategory(props) {
         </div>
       </div>
       <div className="shopcategory-products">
-        
+        {all_products.map((item,i)=>{
+          if (props.category === item.category) {
+            return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
+          }else{
+            return null;
+          }
+
+  })}
       </div>
     </div>
   );
